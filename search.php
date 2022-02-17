8b8a966ea26a925386770b5fe83e34461ac15368
almakkis1 <s2007mk@gmail.com>
2022-02-17 14:34:55 +0300

    تعديلات غير هامة

--- search.php
+++ search.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
 <html>
     <head>
         <title>Scammer Book</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body>
+    <div class="container">
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-          </div>
+            <label for="exampleFormControlInput1" class="form-label">Scammer phone or link:</label>
+            <input class="form-control form-control-lg" type="text" placeholder="Scammer's number or link ..." aria-label=".form-control-lg example">
+        </div>
+    </div>
     </body>
 </html>
