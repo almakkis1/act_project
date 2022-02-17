1f9756d43db494b25a636edb455df0c5d182ff81
almakkis1 <s2007mk@gmail.com>
2022-02-17 15:38:52 +0300

    بعض التعديلات
    إضافة خلفية وضبط موقع محتويات الصفحة

--- search.php
+++ search.php
@@ -2,13 +2,42 @@
     <head>
         <title>Scammer Book</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <style>
+    body {
+    background-image: url('https://www.soudfa.com/content/blog-articles/en-076.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;  
+    background-size: cover;
+
+    height: 200px;
+    width: 400px;
+
+    position: fixed;
+    top: 80%;
+    left: 10%;
+    right: 10%;
+    margin-top: -100px;
+    margin-left: -200px;
+}
+</style>
     </head>
-    <body>
-    <div class="container">
+    <body class="container">
+
+        <div class="row align-items-center">
+        <form class="row g-3">
+    
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Scammer phone or link:</label>
             <input class="form-control form-control-lg" type="text" placeholder="Scammer's number or link ..." aria-label=".form-control-lg example">
         </div>
-    </div>
+        
+        <div class="col-auto">
+            <button type="submit" class="btn btn-primary mb-3">Search</button>
+        </div>
+        <div class="col-auto">
+            <button type="submit" class="btn btn-primary mb-3">Report Scammer</button>
+        </div>
+    
+        </form>
+</div>
     </body>
 </html>
